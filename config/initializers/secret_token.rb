69c9977d6722b289faffd3507ba7a5526004fd2c
Guy Sopher <guysopher@gmail.com>
2013-03-01 10:39:51 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beatrootmusic::Application.config.secret_token = 'e26e1cc5f5f35c3d81cfe1dfb1c60a9da7e032a33d3ad73cfefd6b18fe7be9d830f8d5c69aaca68b96815db928d12042a1ea47d1bd048502228443fc12e23d2f'
