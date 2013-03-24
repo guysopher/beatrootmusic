abaf558b0608e4caed3815ab47e8765a9c4b5025
Guy Sopher <guysopher@gmail.com>
2013-03-24 10:01:15 +0200

    removed resize

--- app/assets/javascripts/body.js
+++ app/assets/javascripts/body.js
@@ -7,6 +7,4 @@
 $(function(){
     wWidth = $(window).width();
     wHeight = $(window).height();
-    $('#twitter').html('<a style="margin:10px 0;" class="twitter-timeline" data-dnt="true" width="' + parseInt((wWidth*0.3)) + '" height="' + parseInt((wWidth*0.5)) + '" href="https://twitter.com/BeatrootNews" data-widget-id="302822508762107904">Tweets by @BeatrootNews</a>');
-    $('#facebook').html('<div style="margin:10px 0;" class="fb-like-box" data-href="http://www.facebook.com/beatrootofficial" data-height="' + parseInt((wWidth*0.5)) + '" data-width="' + parseInt((wWidth*0.3)) + '" data-show-faces="true" data-colorscheme="dark" data-stream="true" data-header="true"></div>');
 });
