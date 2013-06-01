be27d309592921b0b940dd116a36b0633c5c1940
Guy Sopher <guy@moment.me>
2013-06-01 13:24:52 +0300

    redirect to www if needed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :check_uri
+
+  def check_uri
+    redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host)
+  end
+
   def index
     @pages = Page.order('"pages"."order"')
   end
